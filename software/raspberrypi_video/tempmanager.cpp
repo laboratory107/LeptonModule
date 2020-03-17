195020fd656a4b883d2f3951d299533a60d19ca3
Boris Tomas <boris.tomas@gmail.com>
2020-03-17 13:36:16 +0100

    ui updates

---
+++ software/raspberrypi_video/tempmanager.cpp
@@ -0,0 +1,6 @@
+#include "tempmanager.h"
+
+TempManager::TempManager()
+{
+
+}
---
+++ software/raspberrypi_video/tempmanager.h
@@ -0,0 +1,11 @@
+#ifndef TEMPMANAGER_H
+#define TEMPMANAGER_H
+
+
+class TempManager
+{
+public:
+    TempManager();
+};
+
+#endif // TEMPMANAGER_H
